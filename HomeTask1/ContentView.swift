92e81966c639c71a9481092e2be26eb54b2daf82
StasMishen <stasmishenkin@mail.ru>
2021-03-17 20:49:45 +0500

    Initial Commit

---
+++ HomeTask1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeTask1
+//
+//  Created by Стас on 17.03.2021.
+//  Copyright © 2021 com.example. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
